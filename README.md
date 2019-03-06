864cfd41cd02a49532d3c67c2f24d213dc750133
Serban Ghita <sghita@adobe.com>
2019-03-06 17:29:41 +0200

    Improved demo

--- README.md
+++ README.md
@@ -3,6 +3,28 @@
 
 ![](docs/bfs-weight-diagram.png)
 
+Example of a result Map:
+
+```
+Map {
+  '/' => { visited: true, depth: 0, weight: 5, isMissing: false },
+  '/a' => { visited: true, depth: 1, weight: 4, isMissing: false },
+  '/b' => { visited: true, depth: 1, weight: 4, isMissing: false },
+  '/c' => { visited: true, depth: 1, weight: 4, isMissing: false },
+  '/d' => { visited: true, depth: 1, weight: 4, isMissing: false },
+  '/b/a' => { visited: false, depth: 2, weight: 0, isMissing: false },
+  '/d/a' => { visited: false, depth: 2, weight: 0, isMissing: false },
+  '/d/b' => { visited: false, depth: 2, weight: 0, isMissing: false },
+  '/d/c' => { visited: false, depth: 2, weight: 0, isMissing: false },
+  '/d/d' => { visited: false, depth: 2, weight: 0, isMissing: false } }
+```
+
+Example of a result queue Set (aka. non-visited nodes):
+
+```
+Set { '/b/a', '/d/a', '/d/b', '/d/c', '/d/d' }
+```
+
 #### Install & Run
 
 * `npm install`
--- src/demo.ts
+++ src/demo.ts
@@ -1,22 +1,88 @@
 // Bootstrap
-import BFSearch from "./BFSearch";
-import items from "./items";
+import BFSearch, {IListItem} from "./BFSearch";
 
-const bfs = new BFSearch(items);
+const websiteLinks: IListItem[] = [
+    {
+        identifier: "/",
+        children: [ "/", "/a", "/b", "/c", "/d" ],
+    },
+    {
+        identifier: "/a",
+        children: [ "/", "/a", "/b", "/c", "/d" ],
+    },
+    {
+        identifier: "/b",
+        children: [ "/", "/a", "/b", "/b/a", "/c", "/d" ],
+    },
+        {
+            identifier: "/b/a",
+            children: [ "/", "/a", "/b", "/c", "/d" ],
+        },
+            {
+                identifier: "/b/a/a",
+                children: [ "/b/a/a/a" ],
+            },
+                {
+                    identifier: "/b/a/a/a",
+                    children: [],
+                },
+    {
+        identifier: "/c",
+        children: [ "/", "/a", "/b", "/c", "/d" ],
+    },
+    {
+        identifier: "/d",
+        children: [ "/", "/a", "/b", "/c", "/d", "/d/a", "/d/b", "/d/c", "/d/d" ],
+    },
+        {
+            identifier: "/d/a",
+            children: [ "/", "/a", "/b", "/c", "/d", "/d/a/a" ],
+        },
+            {
+                identifier: "/d/a/a",
+                children: [ ],
+            },
+        {
+            identifier: "/d/b",
+            children: [ "/d/b/a" ],
+        },
+            {
+                identifier: "/d/b/a",
+                children: [ "/d/b/a/a", "/d/b/a/b" ],
+            },
+                {
+                    identifier: "/d/b/a/a",
+                    children: [ ],
+                },
+                {
+                    identifier: "/d/b/a/b",
+                    children: [ ],
+                },
+        {
+            identifier: "/d/c",
+            children: [ ],
+        },
+        {
+            identifier: "/d/d",
+            children: [ ],
+        },
+];
+
+const bfs = new BFSearch(websiteLinks);
 bfs.search("/", 1);
 
 console.log(bfs.storage);
-console.log(bfs.queue); //
+console.log(bfs.queue);
 
 // Map {
-//     '/' => { visited: true, depth: 0, weight: 1 },
-//     '/a' => { visited: true, depth: 1, weight: 0 },
-//     '/b' => { visited: true, depth: 1, weight: 0 },
-//     '/c' => { visited: true, depth: 1, weight: 0 },
-//     '/d' => { visited: true, depth: 1, weight: 0 },
-//     '/b/a' => { visited: true, depth: 2, weight: 0 },
-//     '/d/a' => { visited: true, depth: 2, weight: 0 },
-//     '/d/b' => { visited: true, depth: 2, weight: 0 },
-//     '/d/c' => { visited: true, depth: 2, weight: 0 },
-//     '/d/d' => { visited: true, depth: 2, weight: 0 } }
-// Set { '/d/a/a', '/d/b/a' }
+//     '/' => { visited: true, depth: 0, weight: 5, isMissing: false },
+//     '/a' => { visited: true, depth: 1, weight: 4, isMissing: false },
+//     '/b' => { visited: true, depth: 1, weight: 4, isMissing: false },
+//     '/c' => { visited: true, depth: 1, weight: 4, isMissing: false },
+//     '/d' => { visited: true, depth: 1, weight: 4, isMissing: false },
+//     '/b/a' => { visited: false, depth: 2, weight: 0, isMissing: false },
+//     '/d/a' => { visited: false, depth: 2, weight: 0, isMissing: false },
+//     '/d/b' => { visited: false, depth: 2, weight: 0, isMissing: false },
+//     '/d/c' => { visited: false, depth: 2, weight: 0, isMissing: false },
+//     '/d/d' => { visited: false, depth: 2, weight: 0, isMissing: false } }
+// Set { '/b/a', '/d/a', '/d/b', '/d/c', '/d/d' }

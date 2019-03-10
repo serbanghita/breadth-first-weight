9103bc2b26e537814db3ff3c958757a925a8d021
Serban Ghita <sghita@adobe.com>
2019-03-11 00:11:33 +0200

    HTTPLinkSearch working with sequential promises and plugable callback

--- src/HTTPLinkSearch.ts
+++ src/HTTPLinkSearch.ts
@@ -1,134 +1,214 @@
-import {IStorageItem} from "./BFSearch";
+import * as fs from "fs";
+import path from "path";
+import puppeteer from "puppeteer";
+import {URL} from "url";
 
-interface IPageResult {
-    url: string;
-    href: string;
-    httpStatusCode: number;
-    httpResponseHeaders: {};
-    links: string[];
+export function writeToFile(filePath: string, fileName: string, content: string) {
+    const fd = fs.openSync(path.join(filePath, fileName), "w");
+    fs.writeFileSync(fd, content);
+    fs.closeSync(fd);
 }
 
-interface IStoragePage extends IStorageItem {
+function isRootLink(linkObj: URL, knownUrlObj: URL): boolean {
+    const rootPaths = ["", "#", "/"];
+    return (
+        linkObj instanceof URL &&
+        linkObj.hostname === knownUrlObj.hostname &&
+        linkObj.hash === "" &&
+        linkObj.search === "" &&
+        rootPaths.indexOf(linkObj.href) !== -1
+    );
+}
+
+function filterLinks(linksFound: string[], knownUrlObj: URL): string[] {
+    const matchRelativeProtocol = new RegExp(`^\/\/`, "i");
+    const linksFoundFiltered = linksFound
+        .map((linkHref) => {
+            if (typeof linkHref !== "string") {
+                return "";
+            }
+
+            linkHref = linkHref.trim();
+
+            let linkObj;
+            try {
+                // tld Domain URI.
+                linkObj = new URL(linkHref);
+            } catch (e) {
+                // Non-standard URI.
+                if (linkHref.match(matchRelativeProtocol)) {
+                    linkObj = new URL(`${knownUrlObj.protocol}${linkHref}`);
+                } else if (linkHref.substr(0, 1) !== "/") {
+                    linkObj = new URL(`${knownUrlObj.origin}/${linkHref}`);
+                } else {
+                    linkObj = new URL(`${knownUrlObj.origin}${linkHref}`);
+                }
+            }
+
+            if (isRootLink(linkObj, knownUrlObj)) {
+                return "";
+            }
+
+            return linkObj.hostname === knownUrlObj.hostname ? linkObj.href : "";
+        })
+        .filter((linkHref) => linkHref !== "");
+
+    // Remove duplicates.
+    return [...new Set(linksFoundFiltered)];
+
+}
+
+function getAllDOMLinks(): string[] {
+    return Array.prototype.map.call(
+        document.querySelectorAll("a") || [],
+        (linkElement: HTMLAnchorElement) => {
+            return linkElement.getAttribute("href") || "";
+        },
+    ) as string[];
+}
+
+function sequentialFnPromises(tasks: Array<() => Promise<any>>) {
+    return tasks.reduce((promiseChain: Promise<any[]>, currentTask: () => Promise<any>) => {
+        return promiseChain.then((chainResults) =>
+            currentTask().then((currentResult) => [ ...chainResults, currentResult ]),
+        );
+    }, Promise.resolve([]));
+}
+
+export function chunkArray(array: any[], size: number): any[] {
+    if (!array) {
+        return [];
+    }
+    const firstChunk = array.slice(0, size);
+    if (!firstChunk.length) {
+        return array;
+    }
+    return [firstChunk].concat(chunkArray(array.slice(size, array.length), size));
+}
+
+interface IStorageItem {
+    status: number;
+    visited: boolean;
+    isMissing: boolean;
+    weight: number;
+    depth: number;
+}
+
+interface IFnCallbackResponse {
     url: string;
-    href: string;
-    httpStatusCode: number;
-    httpResponseHeaders: {};
-    linksFound: number;
+    status: number;
+    children: string[];
 }
 
 export default class HTTPLinkSearch {
-    public storage: Map<string, IStoragePage> = new Map([]);
+
     public queue: Set<string> = new Set([]);
+    public storage: Map<string, IStorageItem> = new Map([]);
+    private knownUrlString: string;
+    private knownUrlObj: URL;
+    private fnArray: Array<() => Promise<IFnCallbackResponse>> = [];
+    private depth: number = 0;
 
-    public async findPage(identifier: string): Promise<IPageResult> {
-        const pageResult = await this.fetchPageResult(identifier);
-
-        return pageResult;
+    constructor(url: string) {
+        this.knownUrlString = url;
+        this.knownUrlObj = new URL(url);
+        this.queue.add(url);
     }
 
-    public clear() {
-        this.storage.clear();
-        this.queue.clear();
-    }
-
-    public async search(identifier: string, maxDepth: number) {
-
-        this.clear();
+    public search(maxDepth: number, fnCallback: (url: string, knownUrlObj: URL) => Promise<IFnCallbackResponse>): Promise<any> {
 
         const storage = this.storage;
         const queue = this.queue;
 
-        let depth: number = 0;
-
-        queue.add(identifier);
-
-        while (queue.size > 0) {
-            if (depth > maxDepth) {
-                break;
-            }
-
-            [...queue].forEach(async (itemIdentifier: string) => {
-                // Fetch data from item.
-                const item = await this.findPage(itemIdentifier);
-
-                console.log(itemIdentifier);
-                console.log(item);
-
-                // Missing pages are saved and removed from queue.
-                if (item.httpStatusCode >= 400) {
-                    storage.set(itemIdentifier, {
-                        url: item.url,
-                        href: item.href,
-                        httpStatusCode: item.httpStatusCode,
-                        httpResponseHeaders: item.httpResponseHeaders,
-                        linksFound: 0,
-                        visited: true,
-                        isMissing: true,
-                        depth,
-                        weight: 0,
-                    });
-                    queue.delete(itemIdentifier);
-                    return;
-                }
-
-                // Save to storage.
-                storage.set(itemIdentifier, {
-                    url: item.url,
-                    href: item.href,
-                    httpStatusCode: item.httpStatusCode,
-                    httpResponseHeaders: item.httpResponseHeaders,
-                    linksFound: item.links.length,
-                    visited: true,
-                    isMissing: false,
-                    depth,
-                    weight: 0,
-                });
-
-                // Delete from queue.
-                queue.delete(itemIdentifier);
-
-                // Add only new items to queue.
-                // Update the weight for existing ones.
-                item.links.forEach((childItemIdentifier: string) => {
-                    const childItem = storage.get(childItemIdentifier);
-                    if (childItem) {
-                        childItem.weight += 1;
-                        storage.set(childItemIdentifier, childItem);
-                    } else {
-                        storage.set(childItemIdentifier, {
-                            url: childItemIdentifier,
-                            href: "",
-                            httpStatusCode: 0,
-                            httpResponseHeaders: {},
-                            linksFound: 0,
-                            visited: false,
-                            isMissing: false,
-                            depth: depth + 1,
-                            weight: 0,
-                        });
-                    }
-                });
-            });
-
-            depth += 1;
-
+        if (queue.size === 0) {
+            return Promise.reject(`Queue is empty. Add something to queue before searching.`);
         }
 
-        return;
+        if (this.depth > maxDepth) {
+            return Promise.reject(`Maximum depth of ${maxDepth} reached.`);
+        }
 
-    }
-
-    private fetchPageResult(url: string): Promise<IPageResult> {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve({
-                    url,
-                    href: "/",
-                    httpStatusCode: 200,
-                    httpResponseHeaders: {},
-                    links: ["/a", "/b"],
-                });
-            }, Math.floor(Math.random() * Math.floor(1000)));
+        this.fnArray = [...queue].map((nodeInQueue) => {
+            queue.delete(nodeInQueue);
+            return () => fnCallback(nodeInQueue, this.knownUrlObj);
         });
+
+        const fnArrayBatches = chunkArray(this.fnArray, 2);
+
+        const fnSequenceBatches = fnArrayBatches.map((fnArrayBatch) => {
+            return () => sequentialFnPromises(fnArrayBatch).then((result) => {
+                result.forEach((nodeResult: IFnCallbackResponse) => {
+                    const nodeResultRecord = storage.get(nodeResult.url);
+
+                    if (nodeResultRecord) {
+                        nodeResultRecord.status = nodeResult.status;
+                        nodeResultRecord.visited = true;
+                        storage.set(nodeResult.url, nodeResultRecord);
+                    } else {
+                        storage.set(nodeResult.url, {
+                            status: nodeResult.status,
+                            visited: true,
+                            isMissing: false,
+                            weight: 0,
+                            depth: this.depth,
+                        });
+                    }
+
+                    nodeResult.children.forEach((nodeChild: string) => {
+                        const nodeChildRecord = storage.get(nodeChild);
+                        if (nodeChildRecord) {
+                            nodeChildRecord.weight += 1;
+                            storage.set(nodeChild, nodeChildRecord);
+                        } else {
+                            storage.set(nodeChild, {
+                                status: 0,
+                                visited: false,
+                                isMissing: false,
+                                weight: 0,
+                                depth: this.depth + 1,
+                            });
+                            queue.add(nodeChild);
+                        }
+                    });
+                });
+            });
+        });
+
+        return sequentialFnPromises(fnSequenceBatches).then(() => {
+            this.depth += 1;
+
+            if (queue.size > 0) {
+                return this.search(maxDepth, fnCallback).catch((err) => {
+                    console.log(err);
+                });
+            }
+        });
+
     }
 }
+
+const ls = new HTTPLinkSearch("https://www.mercedes-benz.ro/");
+ls.search(1, async (url: string, knownUrlObj: URL) => {
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1920, height: 1080});
+    await page.goto(url, { waitUntil: ["load", "domcontentloaded", "networkidle0"] });
+    await page.waitForSelector("body");
+    let links: string[] = await page.evaluate(getAllDOMLinks);
+    await page.close();
+    await browser.close();
+
+    links = filterLinks(links, knownUrlObj);
+
+    console.log(url, "\n", "Links found on page:", links, "\n");
+
+    return {
+        url,
+        status: 200,
+        children: links,
+    };
+}).then(() => {
+    console.log("Done! Writing files ...");
+    writeToFile(process.cwd(), "storage.json", JSON.stringify([...ls.storage], null, "  "));
+    writeToFile(process.cwd(), "queue.json", JSON.stringify([...ls.queue], null, "  "));
+});
---
+++ src/experiments.ts
@@ -0,0 +1,76 @@
+//
+// function myPromise(r: string) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve(r);
+//         }, Math.floor(Math.random() * Math.floor(1000)));
+//     });
+// }
+//
+// // const myTasks = [
+// //     myPromise("boss1"),
+// //     myPromise("boss2"),
+// //     myPromise("boss3"),
+// //     myPromise("boss4"),
+// //     myPromise("boss5"),
+// //     myPromise("boss6"),
+// //     myPromise("boss7"),
+// //     myPromise("boss8"),
+// //     myPromise("boss9"),
+// //     myPromise("boss10"),
+// // ];
+//
+// // Sequential.
+// export function sequentialPromises(tasks: Array<Promise<any>>): Promise<any[]> {
+//     return tasks.reduce((promiseChain: Promise<any[]>, currentTask: Promise<any>) => {
+//         return promiseChain.then((chainResults) =>
+//             currentTask.then((currentResult) => [ ...chainResults, currentResult ]),
+//         );
+//     }, Promise.resolve([]));
+// }
+//
+// // sequential(myTasks).then((results) => console.log(results));
+// // [ 'boss1',
+// //     'boss2',
+// //     'boss3',
+// //     'boss4',
+// //     'boss5',
+// //     'boss6',
+// //     'boss7',
+// //     'boss8',
+// //     'boss9',
+// //     'boss10' ]
+//
+//
+// // Sequential batch.
+// export function chunkArray(array: any[], size: number): any[] {
+//     if (!array) {
+//         return [];
+//     }
+//     const firstChunk = array.slice(0, size); // create the first chunk of the given array
+//     if (!firstChunk.length) {
+//         return array; // this is the base case to terminal the recursive
+//     }
+//     return [firstChunk].concat(chunkArray(array.slice(size, array.length), size));
+// }
+//
+// // const myTasksBatch = chunkArray(myTasks, 3);
+// // // [ [ Promise { 'boss1' }, Promise { 'boss2' }, Promise { 'boss3' } ],
+// // //     [ Promise { 'boss4' }, Promise { 'boss5' }, Promise { 'boss6' } ],
+// // //     [ Promise { 'boss7' }, Promise { 'boss8' }, Promise { 'boss9' } ],
+// // //     [ Promise { 'boss10' } ] ]
+// // const a: Array<Promise<string[]>> = myTasksBatch.map((batch: Array<Promise<string>>) => {
+// //     return sequentialPromises(batch);
+// // });
+// // console.log(a);
+//
+// // [ Promise { <pending> },
+// // Promise { <pending> },
+// // Promise { <pending> },
+// // Promise { <pending> } ]
+//
+// // sequentialPromises(a).then((results) => console.log(results));
+// // [ [ 'boss1', 'boss2', 'boss3' ],
+// //     [ 'boss4', 'boss5', 'boss6' ],
+// //     [ 'boss7', 'boss8', 'boss9' ],
+// //     [ 'boss10' ] ]
